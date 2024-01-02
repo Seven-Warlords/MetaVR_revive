c3ba14e2c9b5e6b40b4ef739129dbf92e723c73c
iqnrfosem <146904862+iqnrfosem@users.noreply.github.com>
2024-01-02 17:05:34 +0900

    플레이어 위치 및 고유번호 완성

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,11 +6,13 @@ public class Player : MonoBehaviour
 {
     public int myNumber;
     // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        
+        int a = GameManager.instance.netWorkGameManager.currentplayerNum;
+        GameManager.instance.player.gameObject.transform.position = GameManager.instance.spawnpoints[a - 1].position;
+        GameManager.instance.player.gameObject.transform.rotation = GameManager.instance.spawnpoints[a - 1].rotation;
+        GameManager.instance.player.JoinGame();
     }
-
     // Update is called once per frame
     public void JoinGame()
     {
