975a787cda0a0658779547e943ac6e57376cd720
SliverTree <aprkvnem@gmail.com>
2024-01-05 12:39:28 +0900

    에러 메시지 등록 및 보이기

--- Assets/Script/System/StatusManager.cs
+++ Assets/Script/System/StatusManager.cs
@@ -20,22 +20,34 @@ public class StatusManager : MonoBehaviour
         switch (errorStatus)
         {
             case Status.Connect_TimeOut:
+                WriteString("Photon 연결에 실패하셨습니다.\n 연결을 재시도합니다.");
                 errorStatus = Status.None;
                 break;
             case Status.Connected_TimeOut:
+                WriteString("연결이 끊겼습니다.");
                 errorStatus = Status.None;
                 break;
             case Status.CloseRoom:
+                WriteString("현재 플레이 중으로 들어갈 수 없습니다.");
                 errorStatus = Status.None;
                 break;
             default:
                 break;
         }
     }
-    private void WriteString()
+    private void WriteString(string str)
     {
         if (GameManager.instance.playerChase == null)
             return;
-        //GameManager.instance.playerChase.StatusUI
+        GameManager.instance.playerChase.StatusUI.btn.gameObject.SetActive(true);
+        GameManager.instance.playerChase.StatusUI.tMP_Text.gameObject.SetActive(true);
+        StringDelay delay;
+        if (GameManager.instance.playerChase.StatusUI.tMP_Text.gameObject.TryGetComponent<StringDelay>(out delay))
+        {
+            delay.str = str;
+            delay.startTyping = true;
+        }
+
+        return;
     }
 }
