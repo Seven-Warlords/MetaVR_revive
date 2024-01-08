9082c1a9b1bb3da9696aa395545a72263d6d035a
SliverTree <aprkvnem@gmail.com>
2024-01-08 15:52:10 +0900

    revert

--- Assets/Script/System/NetWorkGameManager.cs
+++ Assets/Script/System/NetWorkGameManager.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
+using System;
 
 public class NetWorkGameManager : MonoBehaviourPun, IPunObservable
 {
     [SerializeField]
-    private bool ready=false;
-    public bool Ready { get { return ready; }set { ready = value; } }
+    private bool ready = false;
+    public bool Ready { get { return ready; } set { ready = value; } }
     public GameObject[] pings;
     public int currentplayerNum;
     public int playercount;
+    public int previousPlayerCount;
     public int[] players;
     public Photon.Realtime.Player[] oldplayers;
     public Photon.Realtime.Player[] newplayers;
@@ -20,9 +21,9 @@ public class NetWorkGameManager : MonoBehaviourPun, IPunObservable
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        if(stream.IsWriting)
+        if (stream.IsWriting)
         {
-            stream.SendNext(playercount) ;
+            stream.SendNext(playercount);
             stream.SendNext(currentplayerNum);
         }
         else
@@ -36,18 +37,61 @@ public class NetWorkGameManager : MonoBehaviourPun, IPunObservable
         oldplayers = new Photon.Realtime.Player[4];
         newplayers = new Photon.Realtime.Player[4];
     }
-	public void Update() {
+    public void Update()
+    {
         playercountfun();
-	}
-	public void playercountfun() {
-        playercount = PhotonNetwork.PlayerList.Length;
-	}
-    public string getleftplayer() {
+        // 이전 플레이어 배열에 값 할당
+        oldplayers = new Photon.Realtime.Player[PhotonNetwork.PlayerList.Length];
+        Array.Copy(PhotonNetwork.PlayerList, oldplayers, PhotonNetwork.PlayerList.Length);
 
-        oldplayers = PhotonNetwork.PlayerList;
-        
-        return oldplayers[0].NickName;
-	}
+        if (previousPlayerCount > playercount)
+        {
+            // 플레이어 수가 감소했을 때 실행할 코드 작성
+            Debug.Log("플레이어 수가 감소했습니다!");
+            string leftPlayerName = GetLeftPlayerName();
+            Debug.Log("떠난 플레이어: " + leftPlayerName);
+        }
+        // 플레이어 수가 증가했을 때 처리
+        else if (previousPlayerCount < playercount)
+        {
+            // 플레이어 수가 증가했을 때 실행할 코드 작성
+            Debug.Log("플레이어 수가 증가했습니다!");
+        }
+
+        // 변경된 플레이어 수를 기록
+        previousPlayerCount = playercount;
+    }
+
+
+    public void playercountfun()
+    {
+        playercount = PhotonNetwork.PlayerList.Length;
+    }
+
+    public string GetLeftPlayerName()
+    {
+        // 이전 플레이어 배열과 현재 플레이어 배열을 비교하여 떠난 플레이어 찾기
+        foreach (Photon.Realtime.Player oldPlayer in oldplayers)
+        {
+            bool playerStillInRoom = false;
+            foreach (Photon.Realtime.Player currentPlayer in PhotonNetwork.PlayerList)
+            {
+                if (oldPlayer == currentPlayer)
+                {
+                    playerStillInRoom = true;
+                    break;
+                }
+            }
+            if (!playerStillInRoom)
+            {
+                // 떠난 플레이어의 이름 반환
+                return oldPlayer.NickName;
+            }
+        }
+
+        // 떠난 플레이어가 없으면 빈 문자열 반환
+        return "";
+    }
     // Update is called once per frame
     public void PlayerJoin()
     {
@@ -74,8 +118,8 @@ public class NetWorkGameManager : MonoBehaviourPun, IPunObservable
             PhotonView.Get(this).RPC("CancelPlayer", RpcTarget.All, playerNum);
         }
     }
-
     [PunRPC]
+
     public void NPlayerJoin()
     {
         currentplayerNum++;
@@ -88,6 +132,7 @@ public class NetWorkGameManager : MonoBehaviourPun, IPunObservable
         currentplayerNum--;
         playercount--;
     }
+
     [PunRPC]
     public void StartPlayer()
     {
@@ -118,8 +163,11 @@ public class NetWorkGameManager : MonoBehaviourPun, IPunObservable
             GetChild(1).GetComponent<PT_Ready>().
                 ReadyPly[num].color = Color.red;
     }
-    public void Ping(int pingi, Vector3 position, Quaternion rotation) {
-        switch (pingi) {
+    [PunRPC]
+    public void Ping(int pingi, Vector3 position, Quaternion rotation)
+    {
+        switch (pingi)
+        {
             case 0:
                 Instantiate(pings[0], position, rotation);
                 break;
