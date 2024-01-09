666ce443972ab7e5b7a0176c94967e97bb5540d4
SliverTree <aprkvnem@gmail.com>
2024-01-09 12:43:28 +0900

    no message

--- Assets/Script/BodyTrackking.cs
+++ Assets/Script/BodyTrackking.cs
@@ -5,7 +5,7 @@ using Photon;
 using Photon.Pun;
 public class BodyTrackking : MonoBehaviour
 {
-
+    private PhotonView pv;
     public GameObject mybody;
     private Transform myTR;
 
@@ -15,11 +15,11 @@ public class BodyTrackking : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        pv = GetComponent<PhotonView>();
         myTR = GetComponent<Transform>();
-  
 
-    
+        if (pv.IsMine)
+        {
             switch (humantype){
                 case 1:
                     mybody = GameManager.instance.playerChase.head;
@@ -31,18 +31,18 @@ public class BodyTrackking : MonoBehaviour
                     mybody = GameManager.instance.playerChase.hand2;
                     break;
             }
-        
-        
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-            myMR.enabled = false;
+        if (pv.IsMine)
+        {
+            //myMR.enabled = false;
             myTR.transform.position = mybody.transform.position;
             myTR.transform.rotation = mybody.transform.rotation;
-        
+        }
         
     }
 }
